13c4370c0dba2c6809c30156636d034b1eee1e74
Icaro Temponi <icarohs7@gmail.com>
2023-05-28 16:33:34 -0300

    Add toggleOpen function to SearchAppBarController

--- lib/src/search_app_bar_controller.dart
+++ lib/src/search_app_bar_controller.dart
@@ -4,4 +4,6 @@ class SearchAppBarController {
   final ValueNotifier<bool> isOpen;
 
   SearchAppBarController([bool isSearchOpen = false]) : isOpen = ValueNotifier(isSearchOpen);
+
+  void toggleOpen([bool? isOpen]) => this.isOpen.value = isOpen ?? !this.isOpen.value;
 }
